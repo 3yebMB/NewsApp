423837b696b8794d84b2e73261e0cde3b9786758
m.zuev <m.zuev@azoft.com>
2021-12-07 16:29:51 +0300

    add model

---
+++ app/src/main/java/dev/m13d/newsapp/model/api/RequestApi.kt
@@ -0,0 +1,24 @@
+package dev.m13d.newsapp.model.api
+
+import dev.m13d.newsapp.model.entity.News
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RequestApi {
+
+    @GET("top-headlines")
+    fun getNewsList(
+        @Query("country") country: String,
+        @Query("apiKey") apiKey: String
+    ) : Single<News>
+    @GET("everything")
+    fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = RetrofitClient.API_KEY
+    ): Single<News>
+}
---
+++ app/src/main/java/dev/m13d/newsapp/model/api/RetrofitClient.kt
@@ -0,0 +1,6 @@
+package dev.m13d.newsapp.model.api
+
+object RetrofitClient {
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "api_key"
+}
